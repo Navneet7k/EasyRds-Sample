eed4a31dede0e571d70bca9fc283f2785498fbfd
Navneet7k <krisnavneet.nk@gmail.com>
2016-12-13 21:41:40 +0530

    First commit

---
+++ app/src/main/java/navneet/com/easyroads/MainActivity.java
@@ -0,0 +1,248 @@
+package navneet.com.easyroads;
+
+import android.app.ActionBar;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Boolean isInternetPresent = false;
+    ConnectionDetector cd;
+    AlertDialogManager alert = new AlertDialogManager();
+    GooglePlaces googlePlaces;
+    PlacesList nearPlaces;
+    GPSTracker gps;
+    ProgressDialog pDialog;
+    ListView lv;
+
+
+    ArrayList<HashMap<String, String>> placesListItems = new ArrayList<HashMap<String,String>>();
+
+
+    // KEY Strings
+    public static String KEY_REFERENCE = "reference"; // id of the place
+    public static String KEY_NAME = "name"; // name of the place
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cd = new ConnectionDetector(getApplicationContext());
+
+        //	actionBar=getActionBar();
+        //	actionBar.setDisplayHomeAsUpEnabled(true);
+
+        // Check if Internet present
+        isInternetPresent = cd.isConnectingToInternet();
+        if (!isInternetPresent) {
+            // Internet Connection is not present
+            alert.showAlertDialog(MainActivity.this, "Internet Connection Error",
+                    "Please connect to working Internet connection", false);
+            // stop executing code by return
+            return;
+        }
+
+        // creating GPS Class object
+        gps = new GPSTracker(this);
+
+        // check if GPS location is available
+        if (gps.canGetLocation()) {
+            Log.d("Your Location", "latitude:" + gps.getLatitude() + ", longitude: " + gps.getLongitude());
+        } else {
+            // Can't get user's current location
+            alert.showAlertDialog(MainActivity.this, "GPS Status",
+                    "Couldn't get location information. Please enable GPS",
+                    false);
+            // stop executing code by return
+            return;
+        }
+
+        // Getting listview
+        lv = (ListView) findViewById(R.id.list);
+
+        // button show on map
+      //  btnShowOnMap = (Button) findViewById(R.id.btn_show_map);
+
+        // calling background Async task to load Google Places
+        // After getting places from Google all the data is shown in listview
+        new LoadPlaces().execute();
+
+
+        /**
+         * ListItem click event
+         * On selecting a listitem SinglePlaceActivity is launched
+         * */
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // getting values from selected ListItem
+                String reference = ((TextView) view.findViewById(R.id.reference)).getText().toString();
+
+                // Starting new intent
+                Intent in = new Intent(getApplicationContext(),
+                        SinglePlaceActivity.class);
+
+                // Sending place refrence id to single place activity
+                // place refrence id used to get "Place full details"
+                in.putExtra(KEY_REFERENCE, reference);
+                startActivity(in);
+            }
+        });
+    }
+
+    /**
+     * Background Async Task to Load Google places
+     * */
+    class LoadPlaces extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage(Html.fromHtml("<b>Please Wait</b><br/>Loading Restaurants..."));
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        /**
+         * getting Places JSON
+         * */
+        protected String doInBackground(String... args) {
+            // creating Places class object
+            googlePlaces = new GooglePlaces();
+
+            try {
+
+                String types = "restaurant"; // Listing places, restaurants
+
+                // in meters
+                double radius = 3000; // 3000 i.e, 3 kilometers
+
+                // get nearest places
+                nearPlaces = googlePlaces.search(gps.getLatitude(),
+                        gps.getLongitude(), radius, types);
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after getting all products
+            pDialog.dismiss();
+            // updating UI from Background Thread
+            runOnUiThread(new Runnable() {
+                public void run() {
+
+                    // Get json response status
+                    String status = nearPlaces.status;
+
+                    // Check for all possible status
+                    if(status.equals("OK")){
+                        // Successfully got places details
+                        if (nearPlaces.results != null) {
+
+                            for (Place p : nearPlaces.results) {
+                                HashMap<String, String> map = new HashMap<String, String>();
+
+                                // Place reference won't display in listview - it will be hidden
+                                // Place reference is used to get "place full details"
+                                map.put(KEY_REFERENCE, p.reference);
+
+                                // Place name
+                                map.put(KEY_NAME, p.name);
+
+
+                                // adding HashMap to ArrayList
+                                placesListItems.add(map);
+                            }
+                            // list adapter
+                            ListAdapter adapter = new SimpleAdapter(MainActivity.this, placesListItems,
+                                    R.layout.list_item,
+                                    new String[] { KEY_REFERENCE, KEY_NAME}, new int[] {
+                                    R.id.reference, R.id.name });
+
+                            // Adding data into listview
+                            lv.setAdapter(adapter);
+                        }
+                    }
+                    else if(status.equals("ZERO_RESULTS")){
+                        // Zero results found
+                        alert.showAlertDialog(MainActivity.this, "Near Places",
+                                "Sorry no places found. Try to change the types of places",
+                                false);
+                    }
+                    else if(status.equals("UNKNOWN_ERROR"))
+                    {
+                        alert.showAlertDialog(MainActivity.this, "Places Error",
+                                "Sorry unknown error occured.",
+                                false);
+                    }
+                    else if(status.equals("OVER_QUERY_LIMIT"))
+                    {
+                        alert.showAlertDialog(MainActivity.this, "Places Error",
+                                "Sorry query limit to google places is reached",
+                                false);
+                    }
+                    else if(status.equals("REQUEST_DENIED"))
+                    {
+                        alert.showAlertDialog(MainActivity.this, "Places Error",
+                                "Sorry error occured. Request is denied",
+                                false);
+                    }
+                    else if(status.equals("INVALID_REQUEST"))
+                    {
+                        alert.showAlertDialog(MainActivity.this, "Places Error",
+                                "Sorry error occured. Invalid Request",
+                                false);
+                    }
+                    else
+                    {
+                        alert.showAlertDialog(MainActivity.this, "Places Error",
+                                "Sorry error occured.",
+                                false);
+                    }
+                }
+            });
+
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+
+
+}
---
+++ app/src/main/java/navneet/com/easyroads/PlaceDetails.java
@@ -0,0 +1,23 @@
+package navneet.com.easyroads;
+
+import com.google.api.client.util.Key;
+
+import java.io.Serializable;
+
+
+public class PlaceDetails implements Serializable {
+
+	@Key
+	public String status;
+	
+	@Key
+	public Place result;
+
+	@Override
+	public String toString() {
+		if (result!=null) {
+			return result.toString();
+		}
+		return super.toString();
+	}
+}
