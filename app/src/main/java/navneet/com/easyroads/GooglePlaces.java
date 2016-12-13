105ccb45cd480c1bb2e9754a250795896b6f671f
Navneet7k <krisnavneet.nk@gmail.com>
2016-12-13 21:51:33 +0530

    updates 1

--- app/src/main/java/navneet/com/easyroads/GooglePlaces.java
+++ app/src/main/java/navneet/com/easyroads/GooglePlaces.java
@@ -21,7 +21,7 @@ public class GooglePlaces {
 	private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
 
 	// Google API Key
-	private static final String API_KEY = "AIzaSyBs9bdvkaJn0g3Ei9sWJK_5gcq70Y6pWho"; // place your API key here
+	private static final String API_KEY = "AIzaSyBs9bdvkaJn0g3Ei9sWJK_5gcq70Y6pWho";
 
 
 
